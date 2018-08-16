a15d3c7c0698028da0d5c495c28b5aa1a8d05b0c
jenjnif <jenjnif@hotmail.com>
2018-08-16 12:44:07 +0100

    simple argparse programme

---
+++ argparse_arg.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+# running this programme without arguments displays nothing to stdout
+# running it with the help option:
+#     python3 argparse.py -h
+# returns the help message
