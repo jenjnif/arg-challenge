0aa46d411067bd45dacb3bab0672ca5f5341287f
jenjnif <jenjnif@hotmail.com>
2018-08-16 13:24:39 +0100

    naming a square argument

---
+++ argparse_square.py
@@ -0,0 +1,10 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# by default argparse treats the options given as a string
+# but using type=int will treat the input as an integer
+parser.add_argument("square", help="display square of a given number", type=int)
+parser.parse_args()
+args = parser.parse_args()
+print(args.square**2)
