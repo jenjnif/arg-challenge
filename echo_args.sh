9446d917a3245522425fd3f37faebae8b6645db2
jenjnif <jenjnif@hotmail.com>
2018-08-16 10:10:15 +0100

    python and bash example of interpreting arguments

---
+++ echo_args.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this file does a similar thing to the new_arg.py file but
+# directly in bash rather than using python
+
+echo $@
--- new_arg.py
+++ new_arg.py
@@ -1,7 +1,15 @@
-# creating a new argument for the command line not using argparse
-# use module one, module two and module three
+#!/usr/bin/env python
+
+# writing a python file to interpret arguments i.e from the command line
 
 import sys
+
 print("This is the name of the script: ", sys.argv[0])
+print("The name of the first argument is the same: ", sys.argv[0])
+print("This is the name of the second argument: ", sys.argv[1])
+print("This is the name of the third argument: ", sys.argv[2])
 print("Number of arguments: ", len(sys.argv))
 print("The arguments are: ", str(sys.argv))
+
+for x in sys.argv:
+    print('Argument: ', x)
